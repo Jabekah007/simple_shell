20faa69a34274f94c705908f9af2e788c2973246
Mutinda <benardsmutinda@gmail.com>
2023-04-25 04:54:32 -0700

    lists1 file created

---
+++ lists1.c
@@ -0,0 +1,122 @@
+#include "shell.h"
+
+/**
+ * list_len - Calculate the length of linked list
+ * @h: this is pointer to first node of the list
+ *
+ * Return: the size of list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * list_to_strings - Returns an array of strings containing the values of
+ * @head: this is pointer to first node in the list
+ *
+ * Return: an array of strings
+ */
+char **list_to_strings(list_t *head)
+{
+	list_t *node = head;
+	size_t i = list_len(head), j;
+	char **strs;
+	char *str;
+
+	if (!head || !i)
+		return (NULL);
+	strs = malloc(sizeof(char *) * (i + 1));
+	if (!strs)
+		return (NULL);
+	for (i = 0; node; node = node->next, i++)
+	{
+		str = malloc(_strlen(node->str) + 1);
+		if (!str)
+		{
+			for (j = 0; j < i; j++)
+				free(strs[j]);
+			free(strs);
+			return (NULL);
+		}
+
+		str = _strcpy(str, node->str);
+		strs[i] = str;
+	}
+	strs[i] = NULL;
+	return (strs);
+}
+
+
+/**
+ * print_list - Prints all the elements of a linked list of type list_t
+ * @h: this is pointer to first node
+ *
+ * Return:  The size of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * node_starts_with - Finds the first node in the linked list whose str
+ * @node: pointer to list head of the linked list
+ * @prefix: String to match at the beginning of a node's str property
+ * @c: Character to match immediately following the prefix
+ *
+ * Return: Pointer to the matching node, or NULL if no match is found
+ */
+list_t *node_starts_with(list_t *node, char *prefix, char c)
+{
+	char *p = NULL;
+
+	while (node)
+	{
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * get_node_index - Finds the index of a given node in a linked list.
+ * @head: Pointer to the head of the linked list
+ * @node: pointer to the node whose index is to be found
+ *
+ * Return: The index of the node, or -1 if the node is not found in the list
+ */
+ssize_t get_node_index(list_t *head, list_t *node)
+{
+	size_t i = 0;
+
+	while (head)
+	{
+		if (head == node)
+			return (i);
+		head = head->next;
+		i++;
+	}
+	return (-1);
+}
