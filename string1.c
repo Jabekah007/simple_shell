97f4f7d1f9d9fa97acab2064fc5f8f6f596f5c07
Mutinda <benardsmutinda@gmail.com>
2023-04-25 09:58:22 -0700

    string1 file created

---
+++ string1.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * _strcpy - makes a string copy
+ * @dest: this is the destination
+ * @src: this is the source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup - this duplicates a string
+ * @str: this is the string to duplicate
+ *
+ * Return: the string that was duplicated, pointed to
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_puts - it prints an input string
+ *@str: this is the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _putchar - creates a stdout file with the character c.
+ * @c: The character c to be printed
+ *
+ * Return: If successful 1.
+ * otherwise, -1;  errno is set properly.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
